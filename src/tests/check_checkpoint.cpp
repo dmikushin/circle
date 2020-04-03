fd1b9eedbcda18215e0d7e099cd06120ff321b5e
Dmitry Mikushin <dmitry@kernelgen.org>
2020-04-03 23:59:27 +0200

    Getting check_checkpoint test to compile

--- src/tests/check_checkpoint.cpp
+++ src/tests/check_checkpoint.cpp
@@ -2,44 +2,40 @@
 #include "queue.hpp"
 #include <check.h>
 #include <stdlib.h>
+#include <string>
 
 START_TEST(test_checkpoint_single_read_write) {
-  const char *test_string = "Here's a test string!";
-  char result[CIRCLE_MAX_STRING_LEN];
+  const std::string test_string = "Here's a test string!";
+  std::string result;
   int fakerank = 5;
 
   int checkpoint_write_ret = -1;
   int checkpoint_read_ret = -1;
 
-  circle::init(0, NULL, circle::RuntimeDefaultFlags);
+  circle::init(0, NULL);
+  circle::internal::Queue out_q(NULL);
 
-  ::circle::internal::Queue *out_q;
-  out_q = ::circle::internal::Queue::init();
-  ::circle::internal::Queue::push(out_q, test_string);
-
-  checkpoint_write_ret = ::circle::internal::Queue::write(out_q, fakerank);
+  out_q.push(test_string);
+  checkpoint_write_ret = out_q.write(fakerank);
   fail_unless(checkpoint_write_ret > 0,
               "The checkpoint write function did not return a positive value.");
 
-  ::circle::internal::Queue::free(out_q);
   circle::finalize();
 
-  circle::init(0, NULL, circle::RuntimeDefaultFlags);
-  ::circle::internal::Queue *in_q;
-  in_q = ::circle::internal::Queue::init();
+  circle::init(0, NULL);
+  circle::internal::Queue in_q(NULL);
 
-  checkpoint_read_ret = ::circle::internal::Queue::read(in_q, fakerank);
+  checkpoint_read_ret = in_q.read(fakerank);
   fail_unless(checkpoint_read_ret > 0,
               "The checkpoint read function did not return a positive value.");
 
-  ::circle::internal::Queue::pop(in_q, result);
-  fail_unless(in_q->count == 0,
+  in_q.pop(result);
+  fail_unless(in_q.getCount() == 0,
               "Queue count was not correct after poping the last element.");
 
-  fail_unless(strcmp(test_string, result) == 0,
+  fail_unless(test_string == result,
               "Result poped from the queue does not match original.");
 
-  ::circle::internal::Queue::free(in_q);
   circle::finalize();
 }
 END_TEST
@@ -67,3 +63,4 @@ int main(void) {
 
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
+
